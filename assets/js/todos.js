274f5895a4ec18175f6bc08674cb81af572d8376
Kim Cyrus Esquibel <kimcyruse@Kims-MacBook-Air.local>
2019-11-21 19:05:54 -0800

    Add a functionality to add new TODO items
    Created a functionality that add a TODO item to the li when a text is entered to the input. Used append() to append a html string to the element we selected. And adding ul $("ul").on("click") with a second argument of "li" that specifies lis that may or may not have been on the page when it loaded inside the ul that was already exist when the page loaded

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,13 +1,16 @@
-// CHECKS OFF ANY TODO
+// CHECKS OFF ANY TODO ITEM
 // turns the todo items gray and add a strikethrough effect
-$("li").on("click", function(){
+// using on click listener to the entire ul parent element that already exist when the page load
+// with a second argument of "li", so when you click an li inside a ul
+// run this code
+$("ul").on("click", "li", function(){
     // toggle the class .completed on and off the li that we clicked on
     $(this).toggleClass("completed");
 });
 
-// CLICK ON X TO DELETE TODO
+// CLICK ON X TO DELETE TODO ITEM
 // pass an event as a parameter to stop event from bubbling up
-$("span").on("click", function(event){
+$("ul").on("click", "span", function(event){
     // a jQuery method that stop an event from bubbling up - stopPropagation()
     event.stopPropagation();
     // to target the parent element of <span> - parent()
@@ -16,4 +19,18 @@ $("span").on("click", function(event){
     $(this).parent().fadeOut(500, function(){
         $(this).remove();
     });
+});
+
+// ADD A NEW TODO ITEM
+$("input[type='text']").on("keypress", function(event){
+    // to check if enter key is pressed - ENTER which value is 13
+    if(event.which === 13){
+        // grabbing new todo text from input
+        var todoText = $(this).val();
+        // sets input value to empty ""
+        $(this).val("");
+        // create a new li and add to ul
+        // append() - can only take a string of html
+        $("ul").append("<li><span>X</span> " + todoText + " </li>");
+    }
 });
