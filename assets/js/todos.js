3a7172dc0fb0a1f466822fd0e6945a03785cca3b
Kim Cyrus Esquibel <kimcyruse@Kims-MacBook-Air.local>
2019-11-21 17:49:43 -0800

    Add a functionality to remove a TODO item
    By clicking on the x inside a span element, it deletes the TODO item.
    Used stopPropagation to stop event bubbling and used fadeOut event to remove the li element and inserting remove() as a parameter to completely remove the element

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,6 +1,19 @@
-// Check Off Specific Todos By CLicking
+// CHECKS OFF ANY TODO
 // turns the todo items gray and add a strikethrough effect
 $("li").on("click", function(){
-    // toggle the class .completed on the li that we clicked on
+    // toggle the class .completed on and off the li that we clicked on
     $(this).toggleClass("completed");
+});
+
+// CLICK ON X TO DELETE TODO
+// pass an event as a parameter to stop event from bubbling up
+$("span").on("click", function(event){
+    // a jQuery method that stop an event from bubbling up - stopPropagation()
+    event.stopPropagation();
+    // to target the parent element of <span> - parent()
+    // and use fadeOut to remove the element
+    // and insert a parameter to completely remove the element
+    $(this).parent().fadeOut(500, function(){
+        $(this).remove();
+    });
 });
